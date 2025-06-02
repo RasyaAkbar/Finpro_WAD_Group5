54045ad6cb41c137ae9d79c684aa6546f7c1301f
RasyaAkbar <rasyaakbarlptp@gmail.com>
2025-06-03 05:19:43 +0700

    back button for detail

--- resources/views/internship/internship-view-detail.blade.php
+++ resources/views/internship/internship-view-detail.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="container mx-auto px-4 py-8 max-w-4xl">
+    {{-- Back Button --}}
+        <div class="mb-6">
+            <a href="{{ route('internships.index') }}" class="inline-flex items-center text-blue-600 hover:text-blue-800 transition-colors">
+                <i class="fas fa-arrow-left mr-2"></i>
+                Back to Internships
+            </a>
+        </div>
+    </div>
 @endsection
