b8375d9a88278809aea1e2ab7f6e7e4a4fe086c7
Mochamad Diaz Reksa Putra <diazreksaputra@gmail.com>
2025-06-03 03:26:49 +0700

    3rd

--- app/Models/CampusActivity.php
+++ app/Models/CampusActivity.php
@@ -9,18 +9,19 @@ class CampusActivity extends Model
 {
     use HasFactory;
 
+    // Specify the table name if it doesn't follow Laravel's naming convention
     protected $table = 'campus_activities';
 
+    // Define which fields are mass assignable (can be set using create() or update())
     protected $fillable = [
-        'title',
-        'description',
-        'start_date',
-        'end_date',
-        'location',
-        'time',
-        'category',
-        'organizer',
-        'status',   
+        'title',        // Title of the activity
+        'description',  // Description or details about the activity
+        'start_date',   // Activity start date (DATE type)
+        'end_date',     // Activity end date (DATE type)
+        'location',     // Where the activity will take place
+        'time',         // Specific time of the activity
+        'category',     // Category (e.g., seminar, workshop)
+        'organizer',    // Name of the organizer or department
+        'status',       // Status (e.g., scheduled, postponed, canceled)
     ];
-
 }
